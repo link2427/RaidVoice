9fcd28f4d56951cfc4d2be6c039dfa54a1b10a97
Jacob <62169030+link2427@users.noreply.github.com>
2023-01-17 23:43:00 -0600

    Got all API calls working!
    Co-authored-by: Josh Payne <JayPay108@users.noreply.github.com>
    Co-authored-by: aidanneel0@gmail.com <aidanneel0@gmail.com>

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import asyncio
 import json
 import keyboard
-import time
 import requests
 import speech_recognition as sr
 
-
+v_Recognition = ""
 
 r = sr.Recognizer()
 
@@ -14,21 +12,44 @@ while True:
         with sr.Microphone() as source:
             audio = r.listen(source)
         try:
-            print("You said: " + r.recognize_google(audio))
+            v_Recognition = str(r.recognize_google(audio))
+
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
-
- 
-def run_query(query):
-    print ('running query')
-    response = requests.post('https://api.tarkov.dev/graphql', json={'query': query})
-    if response.status_code == 200:
-        print (response.json)
-        return response.json()
     
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
-
-
+        def run_query(query):
+            print ('running query')
+            response = requests.post('https://api.tarkov.dev/graphql', json={'query': query})
+            if response.status_code == 200:
+                data = response.text
+                parse_json = json.loads(data)
+                items = parse_json['data']['items'][0]
+                avg24hPrice = parse_json['data']['items'][0]['avg24hPrice']
+                basePrice = parse_json['data']['items'][0]['basePrice'] 
+                name = parse_json['data']['items'][0]['name']
+                
+                if avg24hPrice == 0:
+                    print(basePrice)
+                else:
+                    print(avg24hPrice)
+                
+                
+                #print(response.json())
+                return response.json()
+            
+            else:
+                raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))  
+        new_query = """
+    {
+        items(name: "%s") {
+            basePrice
+            avg24hPrice
+            name
+            shortName
+        }
+    }
+    """ % v_Recognition
+        
+        run_query(new_query)
