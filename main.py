a631484c877e062f8079f486ff1a2d27825608a8
Jacob <62169030+link2427@users.noreply.github.com>
2023-01-18 00:35:53 -0600

    fixed whitespacing and commented

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import json
 import requests
-
 import pip
 try:
     import keyboard
@@ -14,32 +13,32 @@ except ImportError:
     import speech_recognition as sr
     import pyttsx3
 
-v_Recognition = ""  
 
+v_Recognition = ""  
 r = sr.Recognizer()
 ttsEngine = pyttsx3.init()
 
+# Text to Speech to tell the user the price of the item
 def sayPrice(name, price):
     voiceLine = 'The price of %s is %.0f'%(name, price)
-
-    print('voiceLine')
     ttsEngine.say(voiceLine)
     ttsEngine.runAndWait()
-
+ 
+# Checks if the hotkey is held down
 while True:
     if keyboard.is_pressed('caps lock'):
         with sr.Microphone() as source:
             audio = r.listen(source)
         try:
-            v_Recognition = str(r.recognize_google(audio))
+            v_Recognition = str(r.recognize_google(audio, show_all = False))
 
         except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
+            print("Could not understand audio")
         except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
-    
+            print("Could not request results from speech recognition service; {0}".format(e))
+        # Runs query to tarkov.dev API to check price of items
         def run_query(query):
-            print ('running query')
+            print ('Checking the market...')
             response = requests.post('https://api.tarkov.dev/graphql', json={'query': query})
             if response.status_code == 200:
                 data = response.text
@@ -49,13 +48,12 @@ while True:
                     ttsEngine.say('Item not found')
                     ttsEngine.runAndWait()
                     return
-
+                # Parsing JSON Data
                 items = parse_json['data']['items'][0]
-                
                 avg24hPrice = items['avg24hPrice']
                 basePrice = items['basePrice']
                 name = items['name']
-                
+                # If the item is not on the flea market, say the trader price (baseprice).
                 if avg24hPrice == 0:
                     sayPrice(name, basePrice)
                 else:
@@ -63,14 +61,12 @@ while True:
                         sayPrice(name, basePrice)
                     else:
                         sayPrice(name, avg24hPrice)
-                
-                
-
                 return response.json()
             
             else:
                 raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))  
-        new_query = """
+                # Creates query using the response from the voice recognition
+        new_query = """ 
     {
         items(name: "%s") {
             basePrice
@@ -81,4 +77,4 @@ while True:
     }
     """ % v_Recognition
         
-        run_query(new_query)
+        run_query(new_query) # Runs the query
