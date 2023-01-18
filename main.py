82594b038bbf03c7d5c7722c5dfa7e4bb7d47b1b
Jacob <62169030+link2427@users.noreply.github.com>
2023-01-17 20:48:58 -0600

    fix

---
+++ main.py
@@ -0,0 +1,86 @@
+import websockets
+import asyncio
+import base64
+import json
+import pyaudio
+import keyboard
+import time
+import config
+
+URL = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
+auth_key = config.auth_key
+
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+SAMPLE_RATE = 16000
+
+
+p = pyaudio.PyAudio()
+stream = p.open(
+   frames_per_buffer=FRAMES_PER_BUFFER,
+   rate=SAMPLE_RATE,
+   format=pyaudio.paInt16,
+   channels=1,
+   input=True,
+)
+
+stop_event = asyncio.Event()
+
+async def send_receive():
+   print(f'Connecting websocket to url ${URL}')
+   async with websockets.connect(
+       URL,
+       extra_headers=(("Authorization", auth_key),),
+       ping_interval=5,
+       ping_timeout=20
+   ) as _ws:
+       await asyncio.sleep(0.1)
+       print("Receiving SessionBegins ...")
+       session_begins = await _ws.recv()
+       print(session_begins)
+       print("Sending messages ...")
+       async def send():
+           while True:
+               if stop_event.is_set():
+                   break
+               try:
+                   data = stream.read(FRAMES_PER_BUFFER)
+                   data = base64.b64encode(data).decode("utf-8")
+                   json_data = json.dumps({"audio_data":str(data)})
+                   await _ws.send(json_data)
+               except websockets.exceptions.ConnectionClosedError as e:
+                   print(e)
+                   assert e.code == 4008
+                   break
+               except Exception as e:
+                   assert False, "Not a websocket 4008 error"
+               await asyncio.sleep(0.01)
+          
+           return True
+      
+       async def receive():
+           while True:
+               if stop_event.is_set():
+                   break
+               try:
+                   result_str = await _ws.recv()
+                   print(json.loads(result_str)['text'])
+               except websockets.exceptions.ConnectionClosedError as e:
+                   print(e)
+                   assert e.code == 4008
+                   break
+               except Exception as e:
+                   assert False, "Not a websocket 4008 error"
+      
+       send_result, receive_result = await asyncio.gather(send(), receive())
+
+async def background_task():
+    task = asyncio.create_task(send_receive())
+    await asyncio.sleep(6) # run the task for 6 secs
+    task.cancel()
+# Checking for keyboard input
+while True:
+    if keyboard.is_pressed('capslock'):
+        asyncio.run(background_task())
+    time.sleep(0.1)
